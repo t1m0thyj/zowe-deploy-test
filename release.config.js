f328cf74a6110fed85198d6488ef950bd1dcb80f
Timothy Johnson <timothy.johnson@broadcom.com>
2021-05-17 15:35:10 -0400

    test semantic release with single package
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

---
+++ release.config.js
@@ -0,0 +1,41 @@
+module.exports = {
+    branches: [
+        {
+            name: "master",
+            dependencies: [
+                "@zowe/imperative",
+                "@zowe/perf-timing"
+            ],
+            aliasTags: [
+                "zowe-v1-lts"
+            ]
+        },
+        {
+            name: "next",
+            prerelease: true,
+            dependencies: {
+                "@zowe/imperative": "next",
+                "@zowe/perf-timing": "latest"
+            }
+        },
+        {
+            name: "lts-incremental",
+            range: "2.x",
+            dependencies: [
+                "@brightside/imperative"
+            ]
+        },
+        {
+            name: "lts-stable",
+            range: "1.x",
+            dependencies: [
+                "@brightside/imperative"
+            ]
+        }
+    ],
+    plugins: [
+        "semantic-release-zowe-cli",
+        "@semantic-release/npm",
+        "@semantic-release/git"
+    ]
+};
