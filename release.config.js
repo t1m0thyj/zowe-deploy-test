c021fd2684afc1bf7d2575a5fd677f975478ebc6
Timothy Johnson <timothy.johnson@broadcom.com>
2021-05-18 15:22:19 -0400

    update config for 1.5.3
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

--- release.config.js
+++ release.config.js
@@ -2,7 +2,43 @@ module.exports = {
     branches: [
         {
             name: "master",
-            accept: ["minor", "patch"],
+            level: "minor",
+            dependencies: [
+                "@zowe/perf-timing"
+            ],
+            aliasTags: [
+                "zowe-v1-lts"
+            ]
+        },
+        {
+            name: "next",
+            prerelease: true,
+            dependencies: {
+                "@zowe/perf-timing": "latest"
+            }
+        },
+        {
+            name: "lts-*",
+            level: "patch",
+            dependencies: [
+                "@brightside/imperative"
+            ]
+        }
+    ],
+    plugins: [
+        "semantic-release-zowe-cli",
+        "@semantic-release/npm",
+        "@semantic-release/git"
+    ]
+};
+
+/*
+// Sample release config for Zowe CLI
+module.exports = {
+    branches: [
+        {
+            name: "master",
+            range: "6.x",
             dependencies: [
                 "@zowe/imperative",
                 "@zowe/perf-timing"
@@ -21,14 +57,14 @@ module.exports = {
         },
         {
             name: "lts-incremental",
-            range: "2.x",
+            range: "2.36.x",
             dependencies: [
                 "@brightside/imperative"
             ]
         },
         {
             name: "lts-stable",
-            range: "1.x",
+            range: "1.0.x",
             dependencies: [
                 "@brightside/imperative"
             ]
@@ -40,3 +76,4 @@ module.exports = {
         "@semantic-release/git"
     ]
 };
+*/
