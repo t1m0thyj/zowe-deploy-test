e823f3da8d665299d758343bc5b9f205e48d4d1b
Timothy Johnson <timothy.johnson@broadcom.com>
2022-03-02 08:58:26 -0500

    Try semantic-release stuff
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

--- release.config.js
+++ release.config.js
@@ -1,3 +1,35 @@
+// module.exports = {
+//     branches: [
+//         {
+//             name: "master",
+//             level: "minor"
+//         },
+//         {
+//             name: "next",
+//             prerelease: true
+//         },
+//         {
+//             name: "lts-*",
+//             level: "patch"
+//         }
+//     ],
+//     plugins: [
+//         "@octorelease/changelog",
+//         ["@octorelease/npm", {
+//             aliasTags: {
+//                 "latest": "zowe-v1-lts"
+//             },
+//             smokeTest: true,
+//             tarballDir: "dist"
+//         }],
+//         ["@octorelease/github", {
+//             assets: "dist/*.tgz",
+//             checkPrLabels: true
+//         }],
+//         "@octorelease/git"
+//     ]
+// };
+
 module.exports = {
     branches: [
         {
@@ -14,18 +46,13 @@ module.exports = {
         }
     ],
     plugins: [
-        "@octorelease/changelog",
-        ["@octorelease/npm", {
-            aliasTags: {
-                "latest": "zowe-v1-lts"
-            },
-            smokeTest: true,
+        "semantic-release-zowe-cli",
+        ["@semantic-release/npm", {
             tarballDir: "dist"
         }],
-        ["@octorelease/github", {
-            assets: "dist/*.tgz",
-            checkPrLabels: true
+        ["@semantic-release/github", {
+            assets: "dist/*.tgz"
         }],
-        "@octorelease/git"
+        "@semantic-release/git"
     ]
 };
